bbbaca853def311aa220500570279f8d861acc0a
Neil Houston <neil.a.houston@gmail.com>
2014-10-25 21:56:08 +0100

    Client: Login and obtain an auth token

--- api.go
+++ api.go
@@ -1,9 +1,53 @@
 package vcd
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
 )
 
-func NewClient(token string) {
-	fmt.Println("VCloud Test\n")
+type Client struct {
+	Token string
+	URL string
+	httpClient *http.Client
+	creds string
+} 
+
+func NewClient() (*Client, error) {
+	creds   := os.Getenv("SKYSCAPE_CREDS")
+	creds64 := base64.StdEncoding.EncodeToString([]byte(creds))
+
+	client := Client{
+		Token: "XXXX",
+		URL:	 "https://api.vcd.portal.skyscapecloud.com",
+		httpClient:  http.DefaultClient,
+		creds: creds64,
+	}
+
+	client.login()
+	return &client, nil
 }
+
+func (v *Client) login() error {
+	endpoint, _ := url.Parse(v.URL)
+	endpoint.Path = "/api/sessions"
+	var body io.ReadWriter
+
+	hReq, _ := http.NewRequest("POST", endpoint.String(), body)
+
+	hReq.Header.Set("Accept", "application/*+xml;version=5.5")
+	hReq.Header.Set("Authorization", fmt.Sprintf("Basic %s", v.creds))
+
+	re, _ := v.httpClient.Do(hReq)
+	defer re.Body.Close()
+
+	v.Token = re.Header["X-Vcloud-Authorization"][0]
+	fmt.Println("VCD HTTP STATUS:  ", re.StatusCode)
+	fmt.Println("VCD CREDENTIALS:  ", v.creds)
+	fmt.Println("VCD AUTH TOKEN:   ", v.Token)
+	return nil
+}
+
--- cmd/driver/main.go
+++ cmd/driver/main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	vcd.NewClient("wibble");
+	vcd.NewClient();
 }
