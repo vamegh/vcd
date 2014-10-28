43bf346fed15f639ba15476b1df9ee6cf9100103
Vamegh Hedayati <vhedayati@ev9.eu>
2014-10-28 16:14:53 +0000

    Modified api.go - Added some basic error checking and command line flag handling
    	modified:   api.go

--- api.go
+++ api.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+  "flag"
 )
 
 type Client struct {
@@ -19,10 +20,34 @@ type Client struct {
 func NewClient() (*Client, error) {
 	creds := os.Getenv("VCD_CREDS")
 	url := os.Getenv("VCD_URL")
+
+  flag_url := flag.String("url", "", "Please provide a valid URL")
+  flag_creds := flag.String("cred", "", "Please provide valid Credentials")
+  flag.Parse()
+
+  if url == "" {
+    if *flag_url == "" {
+      fmt.Println("URL Environment not set please set using export VCD_URL or by passing via command line argument --url=",
+                  "\nUrl ENV Currently : ",url, "\nURL Command Line Currently : ",*flag_url)
+      os.Exit(1)
+    } else {
+      url = *flag_url
+    }
+  }
+  if creds == "" {
+    if *flag_creds == "" {
+      fmt.Println("Credentials Environment not set please set using export VCD_CREDS or by passing via command line argument --cred=",
+                  "\nVCD_CREDS Currently : ",creds,"\nCREDS Command Line Currently : ",*flag_creds )
+      os.Exit(1)
+    } else {
+      creds = *flag_creds
+    }
+  }
+
 	creds64 := base64.StdEncoding.EncodeToString([]byte(creds))
 
 	client := Client{
-		Token:      "XXXX",
+		Token:      "FAIL",
 		URL:        url,
 		httpClient: http.DefaultClient,
 		creds:      creds64,
@@ -53,5 +78,11 @@ func (v *Client) login() error {
 }
 
 func (v *Client) ShowToken() {
-	fmt.Println("VCD Token: ", v.Token)
+  if (v.Token == "FAIL") {
+    fmt.Println("Auth Has Failed")
+    os.Exit(1)
+  } else {
+    fmt.Println("VCD Token: ", v.Token)
+  }
 }
+
